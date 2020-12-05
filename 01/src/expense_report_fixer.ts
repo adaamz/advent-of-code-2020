d53a693330b7e71216c26ab69dd5a8898a887149
Adam Žurek <thejabko@gmail.com>
2020-12-05 15:44:48 +0100

    optimized day 1 a bit

--- 01/src/expense_report_fixer.ts
+++ 01/src/expense_report_fixer.ts
@@ -1,9 +1,11 @@
 const neededSumValue = 2020;
 
-export function fixExpenseReport2(expenses: Iterable<number>): number
+export function fixExpenseReport2(expenses: number[]): number
 {
-  for (const expense1 of expenses) {
-    for (const expense2 of expenses) {
+  for (let i = 0; i < expenses.length - 1; i++) {
+    const expense1 = expenses[i];
+    for (let j = i; j < expenses.length; j++) {
+      const expense2 = expenses[j];
       if (expense1 + expense2 === neededSumValue) {
         return expense1 * expense2;
       }
@@ -13,11 +15,14 @@ export function fixExpenseReport2(expenses: Iterable<number>): number
   return NaN;
 }
 
-export function fixExpenseReport3(expenses: Iterable<number>): number
+export function fixExpenseReport3(expenses: number[]): number
 {
-  for (const expense1 of expenses) {
-    for (const expense2 of expenses) {
-      for (const expense3 of expenses) {
+  for (let i = 0; i < expenses.length - 2; i++) {
+    const expense1 = expenses[i];
+    for (let j = i; j < expenses.length - 1; j++) {
+      const expense2 = expenses[j];
+      for (let k = j; k < expenses.length; k++) {
+        const expense3 = expenses[k];
         if (expense1 + expense2 + expense3 === neededSumValue) {
           return expense1 * expense2 * expense3;
         }
